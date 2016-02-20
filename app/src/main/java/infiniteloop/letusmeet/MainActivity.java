fa56aa7cfd74deb0b2bbe13427eb0f280bf865e0
Sharath Pandeshwar <infinitec123@gmail.com>
2016-02-20 17:23:15 +0530

    Commit 4 - Map fixes

--- app/src/main/java/infiniteloop/letusmeet/MainActivity.java
+++ app/src/main/java/infiniteloop/letusmeet/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,10 +39,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(12.9716, 77.5946);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Sharath").icon(BitmapDescriptorFactory.fromResource(R.drawable.pic_sharath)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12));
     }
 
 }
