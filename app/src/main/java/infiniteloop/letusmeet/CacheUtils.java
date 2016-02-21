e5e39b1dfd6c782e42109b285a9c9a36bdab0f02
Sharath Pandeshwar <infinitec123@gmail.com>
2016-02-21 07:35:24 +0530

    Preferences

--- app/src/main/java/infiniteloop/letusmeet/CacheUtils.java
+++ app/src/main/java/infiniteloop/letusmeet/CacheUtils.java
@@ -3,7 +3,6 @@ package infiniteloop.letusmeet;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 public class CacheUtils {
 
@@ -12,20 +11,11 @@ public class CacheUtils {
     /* Reference to shared preference */
     private SharedPreferences mSharedPreferences;
 
-    /**
-     * Keys
-     */
-    private static String sIsGcmRegistrationCompleteKey = "isGcmRegistrationComplete";
-    private static String sUserNameKey = "user_name";
-    private static String sUserProfilePicPath = "user_profile_pic_path";
 
-    private static String sLastKnownLatitudeKey = "last_known_latitude";
-    private static String sLastKnownLongitudeKey = "last_known_longitude";
-    private static String sLoginComplete = "login_complete";
-
-    private static String sCoverPicVersion = "cover_pic_version";
-    private static String sLogoPicVersion = "logo_pic_version";
-    private static String sProfilePicVersion = "profile_pic_version";
+    private static String sTimePreferenceKey = "key_time_preference_key";
+    private static String sLocationPreferenceKey = "key_location_preference_key";
+    private static String sOffersPreferenceKey = "key_offer_preference_key";
+    private static String sSocialPreferenceKey = "key_social_preference_key";
 
 
     public CacheUtils(Context context) {
@@ -33,104 +23,44 @@ public class CacheUtils {
     }
 
     //*********************************************************************
-    // APIs : Login related
+    // APIs : Fields
     //*********************************************************************
 
-    public boolean isUserLoginComplete() {
-        return mSharedPreferences.getBoolean(sLoginComplete, false);
+    public boolean isInterestedInTime() {
+        return mSharedPreferences.getBoolean(sTimePreferenceKey, false);
     }
 
-    public void setUserLoginStatus(final boolean value) {
-        mSharedPreferences.edit().putBoolean(sLoginComplete, value).apply();
-    }
-
-    //*********************************************************************
-    // APIs : GCM Related
-    //*********************************************************************
-
-
-    public boolean isGCMRegistrationComplete() {
-        return mSharedPreferences.getBoolean(sIsGcmRegistrationCompleteKey, false);
-    }
-
-    public void setGCMRegistrationStatus(final boolean value) {
-        mSharedPreferences.edit().putBoolean(sIsGcmRegistrationCompleteKey, value).apply();
-        Log.v(TAG, "Successfully set the registration setting");
-    }
-
-    //*********************************************************************
-    // Registration Related
-    //*********************************************************************
-
-    public void saveUserName(String name) {
-        mSharedPreferences.edit().putString(sUserNameKey, name).apply();
-    }
-
-    public String getUserName() {
-        return mSharedPreferences.getString(sUserNameKey, "Unknown");
+    public void setInterestedInTime(final boolean value) {
+        mSharedPreferences.edit().putBoolean(sTimePreferenceKey, value).apply();
     }
 
 
-    public void saveProfilePicPath(String path) {
-        mSharedPreferences.edit().putString(sUserProfilePicPath, path).apply();
+    public boolean isInterestedInLocation() {
+        return mSharedPreferences.getBoolean(sLocationPreferenceKey, false);
     }
 
-    public String getProfilePicPath() {
-        return mSharedPreferences.getString(sUserProfilePicPath, null);
-    }
-
-
-    //*********************************************************************
-    // Location Related
-    //*********************************************************************
-
-    public void saveLastKnownLocation(double lat, double lng) {
-        mSharedPreferences.edit().putLong(sLastKnownLatitudeKey, Double.doubleToRawLongBits(lat)).apply();
-        mSharedPreferences.edit().putLong(sLastKnownLongitudeKey, Double.doubleToRawLongBits(lng)).apply();
-    }
-
-    public double getLastKnownLatitude() {
-        return Double.longBitsToDouble(mSharedPreferences.getLong(sLastKnownLatitudeKey, Double.doubleToLongBits(0d)));
-    }
-
-    public double getLastKnownLongitude() {
-        return Double.longBitsToDouble(mSharedPreferences.getLong(sLastKnownLongitudeKey, Double.doubleToLongBits(0d)));
-    }
-
-    //*********************************************************************
-    // Cover Image cacheing related
-    //*********************************************************************
-
-    public Long getCoverPicVersion() {
-        return mSharedPreferences.getLong(sCoverPicVersion, 1L);
-    }
-
-    public void incrementCoverPicVersion() {
-        Long currentVersion = getCoverPicVersion();
-        currentVersion++;
-        mSharedPreferences.edit().putLong(sCoverPicVersion, currentVersion).apply();
-    }
-
-    public Long getLogoPicVersion() {
-        return mSharedPreferences.getLong(sLogoPicVersion, 1L);
-    }
-
-    public void incrementLogoPicVersion() {
-        Long currentVersion = getLogoPicVersion();
-        currentVersion++;
-        mSharedPreferences.edit().putLong(sLogoPicVersion, currentVersion).apply();
+    public void setInterestedInLocation(final boolean value) {
+        mSharedPreferences.edit().putBoolean(sLocationPreferenceKey, value).apply();
     }
 
 
-    public Long getProfilePicVersion() {
-        return mSharedPreferences.getLong(sProfilePicVersion, 1L);
+    public boolean isInterestedInOffers() {
+        return mSharedPreferences.getBoolean(sOffersPreferenceKey, false);
     }
 
-    public void incrementProfilePicVersion() {
-        Long currentVersion = getProfilePicVersion();
-        currentVersion++;
-        mSharedPreferences.edit().putLong(sProfilePicVersion, currentVersion).apply();
+    public void setInterestedInOffers(final boolean value) {
+        mSharedPreferences.edit().putBoolean(sOffersPreferenceKey, value).apply();
     }
+
+
+    public boolean isInterestedInSocialUpdates() {
+        return mSharedPreferences.getBoolean(sLocationPreferenceKey, false);
+    }
+
+    public void setInterestedInSocialUpdates(final boolean value) {
+        mSharedPreferences.edit().putBoolean(sLocationPreferenceKey, value).apply();
+    }
+
 
     //*********************************************************************
     // End of class
